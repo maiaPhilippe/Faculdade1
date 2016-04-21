cbbfdab11a09d10e260a3ce512a28047dd77003a
maiaPhilippe <phil.maia@gmail.com>
2016-04-21 00:43:20 -0300

    rename

---
+++ 1stcode.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\nHi!\nMy name is Philippe\nThis is my first code in C\n");
+    
+    return 0;
+}
--- Untitled1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("\nBoa Noite!\nMeu nome eh Philippe\nEste eh meu segundo programa C\n");
-    
-    return 0;
-}
